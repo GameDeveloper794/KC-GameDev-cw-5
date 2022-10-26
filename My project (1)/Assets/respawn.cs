5aa29a590f125d05e58de5dd7f01c6ed63d37d57
محمود عماد غزلان <114683395+GameDeveloper794@users.noreply.github.com>
2022-10-26 20:43:56 +0300

    second game

---
+++ My project (1)/Assets/respawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class respawn : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(0);
+
+
+
+    }
+
+    
+    
+    
+    
+    
+    
+}
